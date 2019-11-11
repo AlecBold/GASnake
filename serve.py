d2e80e2c57acf9257ef9f37886fab4454cf9aada
AlecBold <sanyokboss55@gmail.com>
2019-11-12 00:04:54 +0300

    as

--- serve.py
+++ serve.py
@@ -6,7 +6,7 @@ from index import Model
 from socket import gethostbyname, gethostname
 
 IP = gethostbyname(gethostname())
-PORT_NUMBER = environ["PORT", 8080]
+PORT_NUMBER = environ["PORT"]
 SERVER_ADDRESS = (IP, PORT_NUMBER)
 
 DUMB_FILE_MAP = {
