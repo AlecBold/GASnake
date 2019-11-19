7512d7079d49cc3d44a4fdaa228753f4a09b2f7c
AlecBold <sanyokboss55@gmail.com>
2019-11-19 21:37:10 +0300

    fix?

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import time
 
 PORT_NUMBER = int(environ["PORT"])
 
-
+"""""""""
 class Model:
     def __init__(self):
         self.proc = Process(target=snake_processing.PlaySnake().execute)
@@ -16,12 +16,19 @@ class Model:
 
     def stop_process(self):
         self.proc.join()
+"""""""""
+
+
+class Model:
+
+    def __init__(self):
+        self.proc = snake_processing.PlaySnake()
 
 
 try:
     model = Model()
     print(f'Start model on process: {PORT_NUMBER}')
-    model.run_process()
+    #model.run_process()
 except KeyboardInterrupt:
     print('^C received, shutting down training')
-    model.stop_process()
+    #model.stop_process()
